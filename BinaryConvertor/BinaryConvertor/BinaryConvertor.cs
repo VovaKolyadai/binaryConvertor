bc3928f5cde1b1bd42f7ae6d3efe4f2954cd12fa
VovaKolyadai <medzhigam.games@gmail.com>
2024-12-30 15:56:17 +0100

    finish binary  convertor

---
+++ BinaryConvertor/BinaryConvertor/BinaryConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryConvertor
+{
+    public static class BinaryConvertor
+    {
+        public static double Convert(string code)
+        {
+            double result = 0;
+            int index = 0;
+            for (int i = code.Length - 1; i >= 0; i--)
+            {
+                if (!(code[i].Equals('0') || code[i].Equals('1'))) throw new ArgumentException("code have to contain 1 and 0");
+                result += int.Parse(code[i].ToString()) * Math.Pow(2, index);
+                index++;
+            }
+            return result;
+        }
+    }
+}
---
+++ BinaryConvertor/BinaryConvertor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BinaryConvertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(BinaryConvertor.Convert("100000"));
+        }
+    }
+}
