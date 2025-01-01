2c6c5757263ec5bc9e6ad487e58cb368214fdd8d
VovaKolyadai <medzhigam.games@gmail.com>
2025-01-01 17:59:09 +0100

    made regular expression for even numbers

--- BinaryConvertor/BinaryConvertor/Program.cs
+++ BinaryConvertor/BinaryConvertor/Program.cs
@@ -1,11 +1,17 @@
-﻿namespace BinaryConvertor
+﻿using System.Text.RegularExpressions;
+
+namespace BinaryConvertor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine(BinaryConvertor.Convert("100000"));
+            var conversion = BinaryConvertor.Convert("100000");
+            var pattern = "(0|2|4|6|8)$";
+            Regex reg = new Regex(pattern);
+            var result = reg.Match(conversion.ToString());
+            Console.WriteLine(result.Success);
         }
     }
 }
